2d9030068c9a771f0f0e069aaa1741eb05e86cb0
Jandrajupallinakshatra <jnakshatra551@gmail.com>
2026-02-12 10:55:29 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-class hello
-  {
-    public static void main(String args[])
-    {
-      system.out.println("hello git from java");
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git from Java");
     }
-  }
+}
